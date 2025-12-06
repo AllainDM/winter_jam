a0dfdb64a7bd99251d3ae99d9c6b59adbc432cf7
brown85858585 <151971750+brown85858585@users.noreply.github.com>
2025-12-06 18:52:44 +0300

    fix moving player attck animation

--- script/player_01.gd
+++ script/player_01.gd
@@ -1,18 +1,26 @@
 extends CharacterBody2D
 
 @onready var anim : AnimatedSprite2D = $AnimatedSprite2D
-const SPEED : float = 300.0
+const SPEED : float = 200.0
 var is_attacking : bool = false
+var attack_cooldown_time : float = 0.5
+var attack_cooldown : float = attack_cooldown_time
 
-func _physics_process(_delta: float) -> void:
-	var input_vector : Vector2 = Vector2.ZERO
-	input_vector.x = Input.get_axis("ui_left", "ui_right")
-	input_vector.y = Input.get_axis("ui_up", "ui_down")
+func _physics_process(delta: float) -> void:
 	
-	input_vector = input_vector.normalized()
+	if attack_cooldown > 0:
+		attack_cooldown -= delta
+		if attack_cooldown <= 0:
+			is_attacking = false
+
+	var input_vector : Vector2 = Vector2.ZERO
+	if !is_attacking:
+		input_vector.x = Input.get_axis("ui_left", "ui_right")
+		input_vector.y = Input.get_axis("ui_up", "ui_down")
+		input_vector = input_vector.normalized()
 
 	if is_attacking:
-		pass
+		velocity = Vector2.ZERO
 	elif input_vector != Vector2.ZERO:
 		anim.play("walk_down")
 		velocity = input_vector * SPEED
@@ -26,8 +34,8 @@ func _on_hitbox_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
 		print("player toches enemy")
 		body.die()
+		
 		is_attacking = true
 		anim.play("attack_down")
-		await get_tree().create_timer(2.0).timeout
-		is_attacking = false
+		attack_cooldown = attack_cooldown_time
 	pass
