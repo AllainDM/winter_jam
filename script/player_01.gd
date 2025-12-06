88a874fcec40e0e1fffe4619eb771ec65e8bd752
brown85858585 <151971750+brown85858585@users.noreply.github.com>
2025-12-06 14:54:02 +0300

    player_hit-box_collider

--- script/player_01.gd
+++ script/player_01.gd
@@ -1,25 +1,33 @@
 extends CharacterBody2D
 
+@onready var anim : AnimatedSprite2D = $AnimatedSprite2D
+const SPEED : float = 300.0
+var is_attacking : bool = false
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _physics_process(_delta: float) -> void:
+	var input_vector := Vector2.ZERO
+	input_vector.x = Input.get_axis("ui_left", "ui_right")
+	input_vector.y = Input.get_axis("ui_up", "ui_down")
+	
+	input_vector = input_vector.normalized()
 
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if is_attacking:
+		pass
+	elif input_vector != Vector2.ZERO:
+		anim.play("walk_down")
+		velocity = input_vector * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+		
+	if velocity == Vector2.ZERO:
+		anim.play("idle_down")
 	move_and_slide()
+
+func _on_hitbox_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		body.die()
+		is_attacking = true
+		anim.play("attack_down")
+		await anim.animation_finished
+		is_attacking = false
+	pass
